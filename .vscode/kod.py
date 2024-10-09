c9d9918f3cbd3d5b7134120f4496bb54958a4271
khadi-s-max <lunakhannn01@gmail.com>
2024-10-09 17:08:27 +0500

    AAA

---
+++ .vscode/kod.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify 
+
+app = Flask(__name__)
+
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    return jsonify({"users": ["Alice", "Bob", "Charlie"]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
---
+++ eski_dars_py/with_seconds.py
@@ -0,0 +1,24 @@
+"""matnlar"""
+print("bu matn edi")
+matn="O'zbekiston respublikasi"
+python="python the best programming language"
+print(matn)
+print(python)
+
+""" "" va '' """
+print("o'g'il")
+print('o\'g\'il')
+
+# atributlar \ metodlar
+shaxar="             NamangaN shaXar uychi Tuman    "
+print(shaxar.title()) #birinchi xarfini katta qilib chiqaradi
+print(shaxar.upper()) #barcha xarfini katta qilib chiqaradi
+print(shaxar.capitalize()) #matndagi 1-harfini 1-so'zini  xarfini katta qilib chiqaradi
+print(shaxar.lower()) #barcha xarfini kichik qilib chiqaradi
+
+
+"""strip()"""
+print(shaxar) 
+print(shaxar.strip())                               
+print(shaxar.lstrip())                               
+print(shaxar.rstrip())                               
